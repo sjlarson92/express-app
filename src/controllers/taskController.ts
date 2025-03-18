3cf2d3a656c35d28c03667cba94ece48d539de9c
sasha <sjlarson92@gmail.com>
2025-03-18 19:53:56 -0400

    add logic to controller to handle CRUD logic for Task table

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,21 +1,56 @@
 import {Request, Response} from "express";
+import {PrismaClient} from "@prisma/client";
 
-export const createTask = (req: Request, res: Response) => {
-    res.send('Task Created')
+const prisma = new PrismaClient();
+
+type TaskRequest = {
+    name: string;
+    age: number;
 }
 
-export const getAllTasks = (req: Request, res: Response) => {
-    res.send('Get all Tasks')
+export const createTask = async (req: Request<{}, {}, TaskRequest>, res: Response) => {
+    const {name} = req.body
+
+    const newTask = await prisma.task.create({
+        data: {
+            title: name,
+            userId: 1
+        }
+    })
+    res.status(201).json({message: `Task Created: ${name}`, data: newTask})
 }
 
-export const getTaskById = (req: Request, res: Response) => {
-    res.send('Get task by id')
+export const getAllTasks = async (req: Request, res: Response) => {
+    const allTasks = await prisma.task.findMany()
+
+    res.status(200).send({message: 'Get all Tasks', data: allTasks})
 }
 
-export const updateTask = (req: Request, res: Response) => {
-    res.send('Update task by id')
+export const getTaskById = async (req: Request, res: Response) => {
+    const {id} = req.params
+    const task = await prisma.task.findUnique({where: {id: parseInt(id)}})
+    res.status(200).send({
+        message: `Get task with id: ${id}`, data: task
+    })
 }
 
-export const deleteTask = (req: Request, res: Response) => {
-    res.send('Delete task by id')
+export const updateTask = async (req: Request<{ id: string }, {}, TaskRequest>, res: Response) => {
+    const {id} = req.params
+    const {name} = req.body
+    const updatedTask = await prisma.task.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: {
+            title: name,
+        }
+    })
+
+    res.send({message: `Update task by id: ${id}`, data: updatedTask})
+}
+
+export const deleteTask = async (req: Request, res: Response) => {
+    const {id} = req.params
+    await prisma.task.delete({where: {id: parseInt(id)}})
+    res.send({message: `Delete task by id: ${id}`})
 }
